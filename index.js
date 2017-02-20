dfd542ee612eaf64c642d81908cd70005f4e5ea0
Pooja Saxena <pooja335@gmail.com>
2017-02-19 19:48:15 -0600

    added event handling

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 'use strict'
 var repos = ['angular/angular.js', 'emberjs/ember.js', 'facebook/react', 'vuejs/vue'] 
-var allData = [];
+var allInfo = {
+  'data': [],
+  'checked': [],
+  'sortedBy': '' 
+};
 
 var fetchCommitActivity = (repo) => {
   return fetch(`https://api.github.com/repos/${repo}/stats/commit_activity?access_token=71e497341966b1d5e8f2651df3f475550baa7da5`, {
@@ -44,38 +48,68 @@ var fetchRepo = (repo) => {
     currentRepoData['commits'] = commitData.reduce((totalCommits, week) => {
       return totalCommits + week.total;
     }, 0)
-    allData.push(currentRepoData);
+    allInfo.data.push(currentRepoData);
   })
 }
 
-var displayData = (allData) => {
+var displayFilters = (allInfo) => {
+  allInfo.data.forEach((framework) => {
+    var checkboxDiv = document.createElement("div");
+    checkboxDiv.className = "checkbox"
+    checkboxDiv.innerHTML = `<label><input type="checkbox" name="${framework.name}">${framework.name}</label>`                       
+    document.getElementById("filter").appendChild(checkboxDiv);
+  })
+}
+
+var displayData = (allInfo) => {
   var resultTbody = document.getElementById("results")
-  var resultRows = document.getElementsByClassName("result-row");
+  var rows = document.getElementsByTagName("tr");
   
-  while (resultRows.length) {
-    resultTbody.removeChild(resultRows[0])
+  while (rows.length) {
+    resultTbody.removeChild(rows[0])
   }
 
-  allData.forEach((framework) => {
+  var headingTR = document.createElement("tr");
+  headingTR.className = "heading-tr"
+  headingTR.innerHTML = '<th></th><th id="commits">Commits in past year</th><th id="stars">Stars</th><th id="issues">Issues</th>'
+  resultTbody.appendChild(headingTR);
+
+  allInfo.data.forEach((framework) => {
     var tr = document.createElement("tr");
-    tr.className = "result-row"
     tr.innerHTML = `<th>${framework.name}</th><td>${framework.commits}</td><td>${framework.stars}</td><td>${framework.issues}</td>`                       
     resultTbody.appendChild(tr);
   })
 }
 
-Promise.all(repos.map(fetchRepo)).then(() => {
-  displayData(allData)
-})
-
-document.getElementById("commits").onclick = (e) => {
-  allData.sort((a, b) => {
-    return (b.commits - a.commits)
+var initialize = () => {
+  document.querySelectorAll(".heading-tr th").forEach((heading) => {
+    heading.onclick = (e) => {
+      allInfo.sortedBy = e.target.id
+      console.log(allInfo)
+      displayData(allInfo)
+    }
   })
-  displayData(allData)
+
+  document.querySelectorAll(".checkbox input").forEach((checkboxInput) => {
+    checkboxInput.onchange = (e) => {
+      var clickedOption = e.target.name;
+      var index = allInfo.checked.indexOf(clickedOption)
+      if (index === -1) {
+        allInfo.checked.push(clickedOption)
+      } else {
+        allInfo.checked.splice(index, 1);
+      }
+      console.log(allInfo)
+      displayData(allInfo)
+    }
+  })
 }
 
-
+Promise.all(repos.map(fetchRepo)).then(() => {
+  displayData(allInfo)
+  displayFilters(allInfo)
+  initialize()
+})
 
 
 
