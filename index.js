9c7164eb75c8309185d73fd293a988c165475a8c
Pooja Saxena <pooja335@gmail.com>
2017-02-21 19:31:27 -0600

    sorting icons

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 'use strict'
-var repos = ['angular/angular.js', 'emberjs/ember.js', 'facebook/react', 'vuejs/vue'] 
+
+var repos = ['angular/angular.js', 'emberjs/ember.js', 'facebook/react', 'vuejs/vue']
+var columnTitles = {
+  commits: 'Commits in Past Year',
+  stars: 'Stars',
+  issues: 'Open Issues'
+}
 var allInfo = {
-  'data': [],
-  'checked': [],
-  'sortedBy': '' 
-};
+  data: [],
+  checked: [],
+  sortedBy: ''
+}
 
 var fetchCommitActivity = (repo) => {
   return fetch(`https://api.github.com/repos/${repo}/stats/commit_activity?access_token=71e497341966b1d5e8f2651df3f475550baa7da5`, {
     method: 'GET',
     headers: new Headers({
-      'Accept': 'application/vnd.github.v3+json'
+      Accept: 'application/vnd.github.v3+json'
     })
   }).then((response) => {
     console.log(response.status)
     if (response.status === 200) {
       return response.json()
     } else if (response.status === 202) {
-      return new Promise((resolve, reject) => {
-        setTimeout(function(){ resolve() }, 100);
+      return new Promise((resolve) => {
+        setTimeout(() => { resolve() }, 100)
       }).then(() => {
         return fetchCommitActivity(repo)
       })
@@ -27,44 +33,52 @@ var fetchCommitActivity = (repo) => {
 }
 
 var fetchRepo = (repo) => {
-  var currentRepoData = {};
+  var currentRepoData = {}
   return fetch(`https://api.github.com/repos/${repo}?access_token=71e497341966b1d5e8f2651df3f475550baa7da5`, {
     method: 'GET',
     headers: new Headers({
-      'Accept': 'application/vnd.github.v3+json'
+      Accept: 'application/vnd.github.v3+json'
     })
   }).then((response) => {
     if (response.status === 200) {
       return response.json()
-    } 
+    }
   }).then((repoData) => {
     currentRepoData = {
-      'name': repoData.name,
-      'stars': repoData.stargazers_count,
-      'issues': repoData.open_issues_count
+      name: repoData.name,
+      stars: repoData.stargazers_count,
+      issues: repoData.open_issues_count
     }
-    return fetchCommitActivity(repo);
+    return fetchCommitActivity(repo)
   }).then((commitData) => {
-    currentRepoData['commits'] = commitData.reduce((totalCommits, week) => {
-      return totalCommits + week.total;
+    currentRepoData.commits = commitData.reduce((totalCommits, week) => {
+      return totalCommits + week.total
     }, 0)
-    return currentRepoData;
+    return currentRepoData
   })
 }
 
+var addHeaderColumn = (headingTR, columnId) => {
+  var headingTH = document.createElement('th')
+  headingTH.innerHTML = `<i id="${columnId}" class="fa ${(allInfo.sortedBy === columnId ? "fa-sort-desc" : "fa-sort")}"></i>&nbsp${columnTitles[columnId]}`
+  headingTR.appendChild(headingTH)
+}
+
 var displayData = () => {
-  var resultTbody = document.getElementById("results")
-  var rows = document.getElementsByTagName("tr");
-  
+  var resultTbody = document.getElementById('results')
+  var rows = document.getElementsByTagName('tr')
+
   while (rows.length) {
     resultTbody.removeChild(rows[0])
   }
 
-  var headingTR = document.createElement("tr");
-  headingTR.className = "heading-tr"
-  headingTR.innerHTML = '<th>Framework</th><th id="commits">Commits in Past Year</th><th id="stars">Stars</th><th id="issues">Open Issues</th>'
-  resultTbody.appendChild(headingTR);
-
+  var headingTR = document.createElement('tr')
+  headingTR.className = 'heading-tr'
+  headingTR.innerHTML = '<th>Framework</th>'
+  addHeaderColumn(headingTR, 'commits')
+  addHeaderColumn(headingTR, 'stars')
+  addHeaderColumn(headingTR, 'issues')
+  resultTbody.appendChild(headingTR)
 
   var filteredSortedData = allInfo.data.filter((framework) => {
     return (allInfo.checked.indexOf(framework.name) > -1)
@@ -75,9 +89,9 @@ var displayData = () => {
   })
 
   filteredSortedData.forEach((framework) => {
-    var tr = document.createElement("tr");
-    tr.innerHTML = `<th>${framework.name}</th><td style="background-color: findRGBvalue()">${framework.commits}</td><td>${framework.stars}</td><td>${framework.issues}</td>`                       
-    resultTbody.appendChild(tr);
+    var tr = document.createElement('tr')
+    tr.innerHTML = `<th>${framework.name}</th><td>${framework.commits}</td><td>${framework.stars}</td><td>${framework.issues}</td>`
+    resultTbody.appendChild(tr)
   })
 }
 
@@ -85,33 +99,32 @@ var displayFilters = () => {
   var frameworkNames = repos.map((repo) => (repo.split('/')[1]))
   frameworkNames.forEach((name) => {
     allInfo.checked.push(name)
-    var checkboxDiv = document.createElement("div");
-    checkboxDiv.className = "checkbox"
-    checkboxDiv.innerHTML = `<label><input type="checkbox" checked name="${name}">${name}</label>`                       
-    document.getElementById("filter").appendChild(checkboxDiv);
+    var checkboxDiv = document.createElement('div')
+    checkboxDiv.className = 'checkbox'
+    checkboxDiv.innerHTML = `<label><input type="checkbox" checked name="${name}">${name}</label>`
+    document.getElementById('filter').appendChild(checkboxDiv)
   })
 }
 
 var initialize = () => {
   displayFilters(allInfo)
 
-  document.getElementById("results").onclick = (e) => {
+  document.getElementById('results').onclick = (e) => {
     if (e.target.id === 'commits' || e.target.id === 'stars' || e.target.id === 'issues') {
       allInfo.sortedBy = e.target.id
       displayData(allInfo)
     }
   }
 
-  document.querySelectorAll(".checkbox input").forEach((checkboxInput) => {
+  document.querySelectorAll('.checkbox input').forEach((checkboxInput) => {
     checkboxInput.onchange = (e) => {
-      var clickedOption = e.target.name;
+      var clickedOption = e.target.name
       var index = allInfo.checked.indexOf(clickedOption)
       if (index === -1) {
         allInfo.checked.push(clickedOption)
       } else {
-        allInfo.checked.splice(index, 1);
+        allInfo.checked.splice(index, 1)
       }
-      console.log(allInfo)
       displayData(allInfo)
     }
   })
@@ -119,7 +132,7 @@ var initialize = () => {
 
 var poll = () => {
   Promise.all(repos.map(fetchRepo)).then((allRepoData) => {
-    allInfo.data = allRepoData;
+    allInfo.data = allRepoData
     displayData()
     setTimeout(poll, 10000)
   })
@@ -127,11 +140,4 @@ var poll = () => {
 
 initialize()
 poll()
-
-
-
-
-
-
-
 
