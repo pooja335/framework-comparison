6fc9954009cb33546ac455af6a9510137eb1aab0
Pooja Saxena <pooja335@gmail.com>
2017-02-22 23:59:15 -0600

    wording changes and constants

---
+++ constants.js
@@ -0,0 +1,5 @@
+module.exports = {
+  repos: ['angular/angular.js', 'emberjs/ember.js', 'facebook/react', 'vuejs/vue'],
+  accessToken: '71e497341966b1d5e8f2651df3f475550baa7da5',
+  pollingFrequency: 10000
+}
